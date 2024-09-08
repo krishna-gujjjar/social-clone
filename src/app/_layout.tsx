2bac792510686d06fc5f9b37a53a6b2c8075f8da
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-09-09 00:49:03 +0530

    refactor: :bento: Custom font added
    General Sans font added as main font and load using expo-font

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,28 @@
+import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
 import '@/styles/global.css';
 
-export default (): JSX.Element => <Slot />;
+SplashScreen.preventAutoHideAsync();
+
+export default (): JSX.Element | null => {
+  const [isFontLoaded, haveFontLoadError] = useFonts({
+    GeneralSansBold: require('@/assets/fonts/GeneralSans-Bold.otf'),
+    GeneralSansSemibold: require('@/assets/fonts/GeneralSans-Semibold.otf'),
+    GeneralSansMedium: require('@/assets/fonts/GeneralSans-Medium.otf'),
+  });
+
+  useEffect(() => {
+    if (isFontLoaded || haveFontLoadError) {
+      SplashScreen.hideAsync();
+    }
+  }, [isFontLoaded, haveFontLoadError]);
+
+  if (!isFontLoaded && !haveFontLoadError) {
+    return null;
+  }
+
+  return <Slot />;
+};
