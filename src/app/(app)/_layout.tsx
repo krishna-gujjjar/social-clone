8f12eb79ff906be9733a5055d7c30d057b3c9ca4
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-09-09 03:30:45 +0530

    chore: :heavy_plus_sign: firebase added.

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,7 +1,10 @@
+import { useStore } from '@/services/storages';
 import { Redirect, Slot } from 'expo-router';
 
 export default (): JSX.Element => {
-  if (true) {
+  const isLoggedIn = useStore(state => state.isLoggedIn);
+
+  if (!isLoggedIn) {
     return <Redirect href="/(auth)/login" />;
   }
 
