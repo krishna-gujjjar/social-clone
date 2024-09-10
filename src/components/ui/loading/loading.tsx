108a183e4d7133756be484d15bb10d4abe47dd25
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-09-10 13:51:25 +0530

    feat: :sparkles: add Loading component with ActivityIndicator

---
+++ src/components/ui/loading/loading.tsx
@@ -0,0 +1,14 @@
+import { memo } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+
+const Component = (): JSX.Element => {
+  return (
+    <View className="absolute top-0 right-0 bottom-0 left-0 z-50 items-center justify-center bg-black/50">
+      <ActivityIndicator className="text-blue-600" size="large" />
+    </View>
+  );
+};
+
+const Loading = memo(Component);
+Loading.displayName = 'Components.UI.Loading';
+export { Loading };
