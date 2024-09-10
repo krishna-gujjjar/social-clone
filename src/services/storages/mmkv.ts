c46e249b10e38125324ef29cb4538b9727450aed
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-09-10 10:36:24 +0530

    refactor: :recycle: added debugging utility function and refactored the MMKV storage initialization.

--- src/services/storages/mmkv.ts
+++ src/services/storages/mmkv.ts
@@ -1,3 +1,26 @@
 import { MMKV } from 'react-native-mmkv';
 
-export default new MMKV();
+const localStorage = new MMKV();
+
+const debugStorage = () => {
+  let ret = '\n-------START OF MMKV DUMP-------\n';
+  const obj: Record<string, string | number | boolean | undefined> = {};
+  for (const key of localStorage.getAllKeys()) {
+    const stringValue = localStorage.getString(key);
+    if (typeof stringValue === 'string' && stringValue.length) {
+      obj[key] = stringValue;
+      continue;
+    }
+    const numberValue = localStorage.getNumber(key);
+    if (typeof numberValue === 'number') {
+      obj[key] = numberValue;
+      continue;
+    }
+    obj[key] = localStorage.getBoolean(key);
+  }
+  ret += JSON.stringify(obj);
+  ret += '\n-------END OF MMKV DUMP-------';
+  console.log(ret);
+};
+
+export { debugStorage, localStorage };
