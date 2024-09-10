6564be4de62f2966c653bc2b528acb18f2e4e8a2
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-09-10 12:17:02 +0530

    feat: :sparkles: add scrollable option to Container component

--- src/components/ui/container/container.tsx
+++ src/components/ui/container/container.tsx
@@ -1,16 +1,18 @@
 import type { Ref } from 'react';
 import { forwardRef, memo } from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { tv } from 'tailwind-variants';
 import type { VariantProps } from 'tailwind-variants';
 
 import type { WithClassName } from '@/types/common';
 
-interface ContainerProps extends WithClassName, VariantProps<typeof contain> {}
+interface ContainerProps extends WithClassName, VariantProps<typeof contain> {
+  scroll?: boolean;
+}
 
 const contain = tv({
-  base: 'h-screen w-full bg-slate-300 p-8 overflow-hidden',
+  base: 'h-screen w-full overflow-hidden bg-slate-300 p-8',
   variants: {
     center: {
       true: 'items-center justify-center',
@@ -22,14 +24,26 @@ const contain = tv({
   },
   defaultVariants: {
     center: false,
+    withoutRounded: false,
   },
 });
 
-const Component = (props: ContainerProps, ref: Ref<View>): JSX.Element => (
-  <SafeAreaView>
-    <View ref={ref} className={contain(props)}>
-      {props.children}
-    </View>
+const Component = (props: ContainerProps, ref: Ref<View | ScrollView>): JSX.Element => (
+  <SafeAreaView className="flex-1">
+    {props.scroll ? (
+      <ScrollView
+        nestedScrollEnabled
+        ref={ref as Ref<ScrollView>}
+        contentContainerClassName={props.className}
+        className={contain({ center: props.center, withoutRounded: props.withoutRounded })}
+      >
+        {props.children}
+      </ScrollView>
+    ) : (
+      <View ref={ref as Ref<View>} className={contain(props)}>
+        {props.children}
+      </View>
+    )}
   </SafeAreaView>
 );
 
