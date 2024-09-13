b7d827e0180cdb481f9903a6f9a66d8f7715cd7b
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-09-13 11:52:09 +0530

    fix: :bug: Add unique key prop to Fragment elements in MetaData component to resolve warning

--- src/components/meta-data/meta-data.tsx
+++ src/components/meta-data/meta-data.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { Col, Inline } from '../ui/container';
 import { Separator } from '../ui/separator';
 import { Tiny, Title } from '../ui/typography';
@@ -10,13 +11,13 @@ interface MetaDatProps {
 export const MetaData = (props: MetaDatProps): JSX.Element => (
   <Inline className="rounded-3xl bg-slate-100 px-6 py-4 shadow-lg shadow-slate-500">
     {props.items.map((item, index) => (
-      <>
-        <Col key={item.id} className="items-center">
+      <Fragment key={`${item.title}-${item.id}`}>
+        <Col className="items-center">
           <Title>{item.value}</Title>
           <Tiny className="text-slate-600">{item.title}</Tiny>
         </Col>
-        {index === props.items.length - 1 || <Separator key={`${item.id}-separator`} />}
-      </>
+        {index === props.items.length - 1 || <Separator />}
+      </Fragment>
     ))}
   </Inline>
 );
