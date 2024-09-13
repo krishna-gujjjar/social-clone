777df5bd6437bf7445a3a0b3204aae566e51be01
Krishna Gujjjar <krishnagujjjar@gmail.com>
2024-09-13 11:49:55 +0530

    refactor: :zap: usePauseable hook to add play and pause callbacks with error handling
    - Introduced `play` and `pause` callbacks using `useCallback` to handle video play and pause actions with error alerts.
     - Added `isLoaded` state to track video load status.
     - Updated `useEffect` and `useLayoutEffect` hooks to utilize the new `play` and `pause` callbacks.
     - Enhanced error handling with custom error messages for different actions.
     - Returned `pause` and `play` functions from the hook for external use.

--- src/hooks/usePauseable.ts
+++ src/hooks/usePauseable.ts
@@ -1,30 +1,67 @@
 import type { Video } from 'expo-av';
 import { useNavigation } from 'expo-router';
 import type { RefObject } from 'react';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useLayoutEffect, useState } from 'react';
+import { Alert } from 'react-native';
 
 export const usePauseable = (video: RefObject<Video>, shouldPlay: boolean) => {
   const navigation = useNavigation();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const play = useCallback(
+    (errorTitle = 'Error on video play') => {
+      if (video.current === null || !isLoaded) return;
+
+      video.current
+        ?.playAsync()
+        .then(() => {})
+        .catch(error => {
+          Alert.alert(errorTitle, JSON.stringify(error?.message));
+        });
+    },
+    [video.current, isLoaded],
+  );
+
+  const pause = useCallback(
+    (errorTitle = 'Error on video pause') => {
+      if (video.current === null || !isLoaded) return;
+      video.current
+        ?.pauseAsync()
+        .then(() => {})
+        .catch(error => {
+          Alert.alert(errorTitle, JSON.stringify(error?.message));
+        });
+    },
+    [video.current, isLoaded],
+  );
 
   useEffect(() => {
     if (video.current === null) return;
 
-    if (shouldPlay) {
-      video.current.playAsync();
-    } else {
-      video.current.pauseAsync();
-      video.current.setPositionAsync(0);
-    }
-  }, [shouldPlay, video.current]);
+    video.current
+      ?.getStatusAsync()
+      .then(status => {
+        setIsLoaded(status.isLoaded);
+      })
+      .catch(console.log);
+  }, [video.current]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    if (shouldPlay) {
+      play('Error on first video play from useLayoutEffect');
+    } else {
+      pause('Error on first video pause from useLayoutEffect');
+    }
+  }, [shouldPlay, play, pause]);
+
+  useLayoutEffect(() => {
     const _blur = navigation.addListener('blur', () => {
-      video.current?.pauseAsync();
+      pause('Error on pause video on blur from useLayoutEffect');
     });
 
     const focus = navigation.addListener('focus', () => {
       if (shouldPlay) {
-        video.current?.playAsync();
+        play('Error on play video on focus from useLayoutEffect');
       }
     });
 
@@ -32,7 +69,7 @@ export const usePauseable = (video: RefObject<Video>, shouldPlay: boolean) => {
       _blur();
       focus();
     };
-  }, [navigation.addListener, shouldPlay, video.current]);
+  }, [navigation.addListener, shouldPlay, play, pause]);
 
-  return video;
+  return { video, pause, play };
 };
